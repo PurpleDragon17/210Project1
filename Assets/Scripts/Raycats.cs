3e20c7e54b1d40299847f82c6f003e67ef5a69a9
PurpleDragon17 <99441260+PurpleDragon17@users.noreply.github.com>
2022-10-17 23:09:35 -0400

    Made Some Progress, still needed work

--- Assets/Scripts/Raycats.cs
+++ Assets/Scripts/Raycats.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Raycats : MonoBehaviour
 {
+    public Transform Eyeballs;
+    public float EyeSpin;
+    
+   
 
 
 
@@ -11,17 +15,32 @@ public class Raycats : MonoBehaviour
     void Start()
     {
 
-    }
 
+      
+    }
     // Update is called once per frame
     void Update()
     {
+        float mouseInputY = Input.GetAxis("Mouse Y");
+        EyeSpin = mouseInputY;
+        //EyeSpin = Mathf.Clamp(EyeSpin, -90f, 90f);
+       // transform.rotation = Quaternion.Euler(new Vector3(EyeSpin, 0f, 0f));
+
+        float mouseInputX = Input.GetAxis("Mouse X");
+        transform.rotation = Quaternion.Euler(transform.eulerAngles + new Vector3(EyeSpin, mouseInputX, 0f));
+       
+
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Yep");
+            RaycastHit hit;
+            if (Physics.Raycast(Eyeballs.position, Eyeballs.forward, out hit))
+            {
+               Transform hip = hit.collider.GetComponent<Transform>();
+                Debug.Log(hip.position);
+
+            }
 
         }
-
     }
 }
 
